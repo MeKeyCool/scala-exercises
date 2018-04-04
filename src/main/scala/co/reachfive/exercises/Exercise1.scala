32929cc6bdea51c87aefa449156fe614605e8df6
Pyc <mekeycool@gmail.com>
2018-04-04 21:48:43 +0200

    Exercise1
    * TODO : check some recursive behavior for flatten method

--- src/main/scala/co/reachfive/exercises/Exercise1.scala
+++ src/main/scala/co/reachfive/exercises/Exercise1.scala
@@ -1,5 +1,16 @@
 package co.reachfive.exercises
 
 object Exercise1 {
-  def flatten(list: List[Any]): List[Any] = ???
+  def flatten(list: List[Any]): List[Any] =
+  {
+    var b: List[Any] = List()
+
+    list foreach {
+      // @TODO: this recursive usage will imply to flatten List(List(List(a))) into List(a); Check if it is expected behavior
+      case l: List[Any] => Exercise1.flatten(l) foreach (a => b = b :+ a)
+      case a: Any => b = b :+ a
+    }
+
+    b
+  }
 }
