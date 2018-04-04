055aa9b79d4fccfc61e0ee9060829b253b142f61
Pyc <mekeycool@gmail.com>
2018-04-04 21:49:12 +0200

    Exercise2

--- src/main/scala/co/reachfive/exercises/Exercise2.scala
+++ src/main/scala/co/reachfive/exercises/Exercise2.scala
@@ -1,5 +1,20 @@
 package co.reachfive.exercises
 
 object Exercise2 {
-  def compress[T](list: List[T]): List[T] = ???
+  def compress[T](list: List[T]): List[T] = {
+
+    var lastElt: Option[T] = None
+    var compressedList: List[T] = List()
+
+    list foreach (
+      t => {
+        if(lastElt.isEmpty || (lastElt.get != t) ) {
+          compressedList :+= t
+          lastElt = Some(t)
+        }
+      }
+      )
+
+    compressedList
+  }
 }
