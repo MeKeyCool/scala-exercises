b8dca4f73367ab6a5e8004982f0350442a98160f
Pyc <mekeycool@gmail.com>
2018-04-06 00:24:57 +0200

    Exercise1 : make it functional

--- src/main/scala/co/reachfive/exercises/Exercise1.scala
+++ src/main/scala/co/reachfive/exercises/Exercise1.scala
@@ -3,14 +3,10 @@ package co.reachfive.exercises
 object Exercise1 {
   def flatten(list: List[Any]): List[Any] =
   {
-    var b: List[Any] = List()
-
-    list foreach {
-      // @TODO: this recursive usage will imply to flatten List(List(List(a))) into List(a); Check if it is expected behavior
-      case l: List[Any] => Exercise1.flatten(l) foreach (a => b = b :+ a)
-      case a: Any => b = b :+ a
+    list match {
+      case Nil => Nil
+      case (x: List[Any]) :: xs => Exercise1.flatten(x) ++ Exercise1.flatten(xs)
+      case x :: xs => x :: Exercise1.flatten(xs)
     }
-
-    b
   }
 }
